9d5519ad381cd195044a0c54834ffb79e9a1e525
Jose Manuel <josemsanchezg511@gmail.com>
2025-06-21 15:35:48 -0500

    modificacion

---
+++ OLIMPIADAS 1/Fecha_Palíndroma_E1.py
@@ -0,0 +1,11 @@
+print("Determina si una fecha es palíndroma".upper())
+
+#
+dia = int(input("Ingrese el dia (1-31)"))
+mes = int(input("Ingrese el mes (1-12)"))
+año = int(input("Ingrese el año (por ejemplo 2020)"))
+fecha = f"{dia:02d}{mes:02d}{año:04d}"
+
+if fecha == fecha[::1]:
+    print(f"Tu fecha {fecha} es palíndroma")
+else: print(f"Tu fecha {fecha} NO es palíndroma")
